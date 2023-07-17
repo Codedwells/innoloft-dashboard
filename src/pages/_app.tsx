96dd983719cc7a271f1fc5ac3d7fb2224b35032e
Abel Misiocha <wells.mail.13@gmail.com>
2023-07-17 14:52:53 +0300

    Update _app.tsx: Remove unused state

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,11 @@
 import store from '@/store';
 import '@/styles/globals.css';
-import { useState } from 'react';
 import { Provider } from 'react-redux';
 import RootLayout from '@/pages/layout';
 import type { AppProps } from 'next/app';
 import { IProductData } from '@/types/types';
 
 export default function App({ Component, pageProps }: AppProps) {
-    const [productData, setProductData] = useState<IProductData | null>(null);
-
     return (
         <Provider store={store}>
             <RootLayout>
